dd53da6170dafc2215ed7b6a5aabbf4ea43728a8
Starkie Phillips <star.phillips03@gmail.com>
2024-11-27 18:26:39 +0000

    form validation

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -4,187 +4,218 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
+	var $window = $(window),
 		$body = $('body'),
 		$sidebar = $('#sidebar');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: [null, '480px']
+	});
 
 	// Hack: Enable IE flexbox workarounds.
-		if (browser.name == 'ie')
-			$body.addClass('is-ie');
+	if (browser.name == 'ie')
+		$body.addClass('is-ie');
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
-	// Forms.
 
-		// Hack: Activate non-input submits.
-			$('form').on('click', '.submit', function(event) {
+	// Regex mail validation function using regex
+	function validateEmail(email) {
+		const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+		return regex.test(email);
+	}
 
-				// Stop propagation, default.
-					event.stopPropagation();
-					event.preventDefault();
+	function validateMessage(message) {
+		return message.trim().length > 0; // Ensure the message is not empty
+	}
 
-				// Submit form.
-					$(this).parents('form').submit();
+	// Ensuring there is a message and a valid domain
+	$('form').on('submit', function (event) {
+		const emailInput = $('#email');
+		const email = emailInput.val();
+		const messageInput = $('#message');
+		const message = messageInput.val();
+
+		// Clear any error messages before validating
+		$('#email-error').remove();
+		$('#message-error').remove();
+
+		let formValid = true; // Flag to check if the form is valid
+
+		// If email is invalid, prevent form submission and show error message
+		if (!validateEmail(email)) {
+			event.preventDefault();
+			const errorMessage = $('<div id="email-error" class="error-message">Please make sure your email address includes a valid domain (like .com, .org, .net, etc.).</div>');
+			emailInput.after(errorMessage);
+			emailInput.focus();
+			formValid = false;  // Set the form as invalid
+		}
+
+		// If message is empty, prevent form submission and show error message
+		if (!validateMessage(message)) {
+			event.preventDefault();
+			const errorMessage = $('<div id="message-error" class="error-message">Please enter a message.</div>');
+			messageInput.after(errorMessage);
+			messageInput.focus();
+			formValid = false;  // Set the form as invalid
+		}
+
+
+	});
+
+	// Sidebar.
+	if ($sidebar.length > 0) {
+
+		var $sidebar_a = $sidebar.find('a');
+
+		$sidebar_a
+			.addClass('scrolly')
+			.on('click', function () {
+
+				var $this = $(this);
+
+				// External link? Bail.
+				if ($this.attr('href').charAt(0) != '#')
+					return;
+
+				// Deactivate all links.
+				$sidebar_a.removeClass('active');
+
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+				$this
+					.addClass('active')
+					.addClass('active-locked');
+
+			})
+			.each(function () {
+
+				var $this = $(this),
+					id = $this.attr('href'),
+					$section = $(id);
+
+				// No section for this link? Bail.
+				if ($section.length < 1)
+					return;
+
+				// Scrollex.
+				$section.scrollex({
+					mode: 'middle',
+					top: '-20vh',
+					bottom: '-20vh',
+					initialize: function () {
+
+						// Deactivate section.
+						$section.addClass('inactive');
+
+					},
+					enter: function () {
+
+						// Activate section.
+						$section.removeClass('inactive');
+
+						// No locked links? Deactivate all links and activate this section's one.
+						if ($sidebar_a.filter('.active-locked').length == 0) {
+
+							$sidebar_a.removeClass('active');
+							$this.addClass('active');
+
+						}
+
+						// Otherwise, if this section's link is the one that's locked, unlock it.
+						else if ($this.hasClass('active-locked'))
+							$this.removeClass('active-locked');
+
+					}
+				});
 
 			});
 
-	// Sidebar.
-		if ($sidebar.length > 0) {
-
-			var $sidebar_a = $sidebar.find('a');
-
-			$sidebar_a
-				.addClass('scrolly')
-				.on('click', function() {
-
-					var $this = $(this);
-
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
-
-					// Deactivate all links.
-						$sidebar_a.removeClass('active');
-
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
-
-				})
-				.each(function() {
-
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
-
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
-
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '-20vh',
-							bottom: '-20vh',
-							initialize: function() {
-
-								// Deactivate section.
-									$section.addClass('inactive');
-
-							},
-							enter: function() {
-
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($sidebar_a.filter('.active-locked').length == 0) {
-
-										$sidebar_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		}
+	}
 
 	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
+	$('.scrolly').scrolly({
+		speed: 1000,
+		offset: function () {
 
-				// If <=large, >small, and sidebar is present, use its height as the offset.
-					if (breakpoints.active('<=large')
-					&&	!breakpoints.active('<=small')
-					&&	$sidebar.length > 0)
-						return $sidebar.height();
+			// If <=large, >small, and sidebar is present, use its height as the offset.
+			if (breakpoints.active('<=large')
+				&& !breakpoints.active('<=small')
+				&& $sidebar.length > 0)
+				return $sidebar.height();
 
-				return 0;
+			return 0;
+
+		}
+	});
+
+	// Spotlights.
+	$('.spotlights > section')
+		.scrollex({
+			mode: 'middle',
+			top: '-10vh',
+			bottom: '-10vh',
+			initialize: function () {
+
+				// Deactivate section.
+				$(this).addClass('inactive');
+
+			},
+			enter: function () {
+
+				// Activate section.
+				$(this).removeClass('inactive');
+
+			}
+		})
+		.each(function () {
+
+			var $this = $(this),
+				$image = $this.find('.image'),
+				$img = $image.find('img'),
+				x;
+
+			// Assign image.
+			$image.css('background-image', 'url(' + $img.attr('src') + ')');
+
+			// Set background position.
+			if (x = $img.data('position'))
+				$image.css('background-position', x);
+
+			// Hide <img>.
+			$img.hide();
+
+		});
+
+	// Features.
+	$('.features')
+		.scrollex({
+			mode: 'middle',
+			top: '-20vh',
+			bottom: '-20vh',
+			initialize: function () {
+
+				// Deactivate section.
+				$(this).addClass('inactive');
+
+			},
+			enter: function () {
+
+				// Activate section.
+				$(this).removeClass('inactive');
 
 			}
 		});
-
-	// Spotlights.
-		$('.spotlights > section')
-			.scrollex({
-				mode: 'middle',
-				top: '-10vh',
-				bottom: '-10vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
-			})
-			.each(function() {
-
-				var	$this = $(this),
-					$image = $this.find('.image'),
-					$img = $image.find('img'),
-					x;
-
-				// Assign image.
-					$image.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Set background position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
-
-				// Hide <img>.
-					$img.hide();
-
-			});
-
-	// Features.
-		$('.features')
-			.scrollex({
-				mode: 'middle',
-				top: '-20vh',
-				bottom: '-20vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
-			});
 
 })(jQuery);
