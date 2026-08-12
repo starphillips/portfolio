5d611348986e99dc9bf2548fabe47d3bc83f0229
Starkie Phillips <star.phillips03@gmail.com>
2026-08-12 12:41:28 +0100

    reestablish and privacy policy

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, request, redirect
 
 import csv
 
+import os
+
 app = Flask(__name__)
 
 
@@ -10,13 +12,23 @@ def my_home():
     return render_template("index.html")
 
 
-@app.route('/<string:page_name>')
+# @app.route('/<string:page_name>')
+# def html_page(page_name):
+#     return render_template(page_name)
+
+@app.route("/<page_name>")
 def html_page(page_name):
+    # if user didn't include .html, add it
+    if not page_name.endswith(".html"):
+        page_name = f"{page_name}.html"
     return render_template(page_name)
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONTACTS_FILE = os.path.join(BASE_DIR, "database.csv")
+print("Using database file at:", CONTACTS_FILE)
 
 def write_to_csv(data):
-    with open('database.csv', mode='a') as database2:
+    with open(CONTACTS_FILE, mode='a') as database2:
         name = data["name"]
         email = data["email"]
         message = data["message"]
@@ -32,7 +44,7 @@ def submit_form():
             data = request.form.to_dict()
             write_to_csv(data)
             print(data)
-            return redirect('/thankyou.html')
+            return redirect('/thankyou')
         except:
             return 'Unable to save to database'
     else:
